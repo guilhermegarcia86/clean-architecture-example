e4259f7a00e2165d6112a149099f844a8540ee6b
Guilherme <guilherme.jose@wirecard.com>
2020-06-06 15:22:24 -0300

    Manual app with in memory database

--- application/manual-app/src/main/java/com/gogo/powerrangers/config/ManualConfig.java
+++ application/manual-app/src/main/java/com/gogo/powerrangers/config/ManualConfig.java
@@ -1,24 +1,18 @@
 package com.gogo.powerrangers.config;
 
-import com.gogo.powerrangers.HibernateUserRepository;
 import com.gogo.powerrangers.db.InMemoryUserRepository;
 import com.gogo.powerrangers.usecase.CreateUser;
 import com.gogo.powerrangers.usecase.SearchUser;
-import com.gogo.powerrangers.usecase.port.UserRepository;
 
 public class ManualConfig {
 
     private final InMemoryUserRepository dataBase = new InMemoryUserRepository();
-    
-	public final UserRepository repository() {
-		return new HibernateUserRepository();
-	}
 
     public CreateUser createUser(){
-        return new CreateUser(repository());
+        return new CreateUser(dataBase);
     }
     
     public SearchUser searchUser() {
-    	return new SearchUser(repository());
+    	return new SearchUser(dataBase);
     }
 }
